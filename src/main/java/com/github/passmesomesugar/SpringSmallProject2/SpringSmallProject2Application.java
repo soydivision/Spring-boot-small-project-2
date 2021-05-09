6091c70d81a5a844ddc344c8756e1457ef23fcdc
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-05-09 13:27:45 +0600

    genesis

---
+++ src/main/java/com/github/passmesomesugar/SpringSmallProject2/SpringSmallProject2Application.java
@@ -0,0 +1,13 @@
+package com.github.passmesomesugar.SpringSmallProject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSmallProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSmallProject2Application.class, args);
+	}
+
+}
